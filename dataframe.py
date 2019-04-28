2804a3f6ab441ea4c12155a7cff2b45372533739
Frank Chen <frankchen1698@gmail.com>
2019-04-28 15:00:43 -0700

    Add datafram.py and preliminary unit tests

---
+++ dataframe.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import sys
+
+
+def validate(file_name):
+    df = pd.read_csv(file_name)
+    df_correct = pd.read_csv('data.csv')
+    try:
+        if (sorted(list(df)) != sorted(list(df_correct))):
+            raise ValueError()
+    except (ValueError):
+        print('Input DataFrame does not contain the expected columns.')
+        exit()
+    try:
+        if (np.any(df_correct.dtypes != df.dtypes)):
+            raise ValueError()
+    except (ValueError):
+        print('Input DataFrame columns do not contain the correct data type.')
+        exit()
+    try:
+        if len(df.index) < 1:
+            raise ValueError()
+    except (ValueError):
+        print("Input DataFrame does not have at least 1 row.")
+        exit()
+    print('Congratulations, input dataframe passes all checks.')
+
+if __name__ == '__main__':
+    validate(sys.argv[1])
---
+++ test_dataframe.py
@@ -0,0 +1,27 @@
+from subprocess import check_output
+import unittest
+
+
+class MyTest(unittest.TestCase):
+    def test_expected_correct_col(self):
+        out = check_output(["python3", "dataframe.py", "data.csv"])
+        self.assertEqual(out, b'Congratulations, \
+                         input dataframe passes all checks.\n')
+
+    def test_expected_correct_types(self):
+        out = check_output(["python3", "dataframe.py", "data.csv"])
+        self.assertEqual(out, b'Congratulations, \
+                         input dataframe passes all checks.\n')
+
+    def test_expected_1_row(self):
+        out = check_output(["python3", "dataframe.py", "data.csv"])
+        self.assertEqual(out, b'Congratulations, \
+                         input dataframe passes all checks.\n')
+
+    def test_incorrect_col(self):
+        out = check_output(["python3", "dataframe.py",
+                           "data_incorrect_col.csv"])
+        self.assertEqual(out, b'Input DataFrame does \
+                         not contain the expected columns.\n')
+if __name__ == '__main__':
+    unittest.main()
